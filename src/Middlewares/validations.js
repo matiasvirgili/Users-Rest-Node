11cc94e92997621872f23aa29bc499925c55bc32
Matias Virgili <matiasvirgili@gmail.com>
2021-10-13 15:13:28 -0300

    create validations

---
+++ src/Middlewares/validations.js
@@ -0,0 +1,22 @@
+const { body } = require('express-validator');
+const validationStringContainNumbers = require('../Helpers/validationStringContainNumbers');
+
+const validations = () => {
+  return [
+    body('name', 'Name is required').notEmpty().isString().trim(),
+    body('lastName', 'Last name is required').notEmpty().isString().trim(),
+    body('telephone', 'Telephone is required')
+      .notEmpty()
+      .isString()
+      .trim()
+      .custom(validationStringContainNumbers),
+    body('direction', 'Direction  is required').notEmpty().isString().trim(),
+    body('dni', 'Dni is required')
+      .notEmpty()
+      .isString()
+      .trim()
+      .custom(validationStringContainNumbers),
+  ];
+};
+
+module.exports = validations;
