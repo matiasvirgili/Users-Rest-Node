c27e0defa8f9e75977900fe33cd7b1f9480b0aab
Matias Virgili <matiasvirgili@gmail.com>
2021-11-24 16:56:25 -0300

    installed cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { json } = require('body-parser');
-
+const cors = require('cors');
 const routerUsers = require('./Router/routeUsers');
 
 require('dotenv').config();
@@ -8,11 +8,16 @@ require('dotenv').config();
 const { ConnectionDB } = require('./DataBase/config');
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
 
 app.use(json());
 
 app.get('/', (req, res) => {
-  res.type('text/plain').send('Server OK, use "https://users-rest-node.herokuapp.com/users" to redirect users');
+  res
+    .type('text/plain')
+    .send(
+      'Server OK, use "https://users-rest-node.herokuapp.com/users" to redirect users'
+    );
 });
 
 app.use('/users', routerUsers);
