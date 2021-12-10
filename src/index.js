e7502395d08d3fb2816b26cfa3af7132665e51a1
Matias Virgili <matiasvirgili@gmail.com>
2021-12-10 17:44:39 -0300

    login finished

--- src/index.js
+++ src/index.js
@@ -2,36 +2,12 @@ const express = require('express');
 const { json } = require('body-parser');
 const cors = require('cors');
 const routerUsers = require('./Router/routeUsers');
-const morgan = require('morgan');
-const passport = require('passport');
-const session = require('express-session')
-const flash = require('connect-flash');
-
+const seeder = require('./Helpers/seeder');
 require('dotenv').config();
-require('./passport/local-auth');
 
 const { ConnectionDB } = require('./DataBase/config');
 const port = process.env.PORT || 3000;
 const app = express();
-
-
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(session({
-  secret: 'mysecretsession',
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(flash())
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use((req, res, next) =>{
-  app.locals.signupMessage = req.flash('singupMessage');
-  app.locals.signinMessage = req.flash('singinMessage');
-  app.locals.user = req.userlogin;
-  next();
-});
 
 app.use(cors());
 app.use(json());
@@ -49,4 +25,5 @@ app.use('/users', routerUsers);
 app.listen(port, async () => {
   console.log(`Server running on http://localhost:${port}`);
   await ConnectionDB();
+  await seeder();
 });
